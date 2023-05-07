afd6170665e6173edda755b5f0dd740fbe2658e3
ElenaSukhina1 <lenanikitina1@yandex.ru>
2023-05-07 15:43:34 +0300

    fix: fix problems for Eslint

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,27 +4,27 @@ const { stdin, stdout } = process;
 
 
 fs.open(path.join(__dirname, 'text.txt'), 'w', (err) => {
-    if(err) throw err;
+  if (err) throw err;
 });
 
 stdout.write('Write your text\n');
 
 function createFile() {
-    stdin.on('data', data => {
-        let string = data.toString();
-        if(string == 'exit\r\n'){
-            process.exit();
-        } else {
-            fs.appendFile(
-                        path.join(__dirname, 'text.txt'),
-                        data,
-                        err => {
-                            if (err) throw err;
-                            // console.log('Файл был изменен');
-                        }
-                    ); 
+  stdin.on('data', data => {
+    let string = data.toString();
+    if (string == 'exit\r\n') {
+      process.exit();
+    } else {
+      fs.appendFile(
+        path.join(__dirname, 'text.txt'),
+        data,
+        err => {
+          if (err) throw err;
+          // console.log('Файл был изменен');
         }
-      });
+      );
+    }
+  });
 }
-createFile()
+createFile();
 
