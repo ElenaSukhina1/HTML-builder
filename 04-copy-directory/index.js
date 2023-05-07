5e2a4d1e436ffd302ee52c84aebabf65cdd9152e
ElenaSukhina1 <lenanikitina1@yandex.ru>
2023-05-07 21:43:41 +0300

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 
+function copyDir() {
+  let firstFile = path.join(__dirname, 'files');
+  let copyFile = path.join(__dirname, 'files-copy');
+  // console.log(copyFile);
+  // console.log(firstFile);
+  fs.mkdir(copyFile, { recursive: true }, err => {
+    if (err) throw err;
+  });
+  fs.readdir(firstFile, (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach(file => {
+        let filesAdress = path.join(__dirname, 'files', file);
+        let copyAdress = path.join(__dirname, 'files-copy', file);
+        fs.copyFile(filesAdress, copyAdress, err => {
+          if (err) throw err;
+          // console.log('Файл успешно скопирован');
+        });
+      });
+    }
+  });
+}
+ 
+
+copyDir();
